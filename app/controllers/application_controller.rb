f89dc11e2779d58008ac1daff2548975710ed662
Tomas Toledo <trtoledo1977@gmail.com>
2018-10-09 03:44:33 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,5 +22,16 @@ class ApplicationController < Sinatra::Base
   erb :show
   end
 
-  
+  get '/articles/:id/edit' do  #load edit form
+    @article = Article.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do #edit action
+    @article = Article.find_by_id(params[:id])
+    @article.title = params[:title]
+    @article.content = params[:content]
+    @article.save
+    redirect to "/articles/#{@article.id}"
+  end
 end
