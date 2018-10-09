90a42332b3b9e0ba32bae4c9bbe393feee780d8d
Tomas Toledo <trtoledo1977@gmail.com>
2018-10-09 03:36:28 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,9 +10,9 @@ class ApplicationController < Sinatra::Base
   get '/articles/new' do
   erb :new
 end
- 
+
   post '/articles' do
     @article = Article.create(:title => params[:title], :content => params[:content])
-  redirect to "/articles/#{@article.id}"
+    redirect to "/articles/#{@article.id}"
   end
 end
