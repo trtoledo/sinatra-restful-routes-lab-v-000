2a1727ff83b21a332069d9f0f7c33818987b53a0
Tomas Toledo <trtoledo1977@gmail.com>
2018-10-09 03:38:41 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/recipes' do
-    @article = Article.create(:title => params[:title], :content => params[:content])
+    @recipe = Recipe.create(:name => params[:name], :ingredient => params[:ingredient])
     redirect to "/articles/#{@article.id}"
   end
 end
